58be3401350a7b562183e17d16ef2abbbcbebbab
felbenini <felbenini@gmail.com>
2023-07-28 21:03:21 -0300

    Layout and header components initialized

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Header from "../components/header.svelte";
+
+</script>
+<Header />
+<slot />
+
+<style global >
+  @import url('https://fonts.googleapis.com/css2?family=REM:wght@300;500;700&display=swap');
+  :global(*) {
+    font-family: 'REM', sans-serif;
+    margin: 0;
+  }
+  :global(p) {
+    font-weight: 300;
+  }
+  :global(h1) {
+    font-weight: 500;
+  }
+</style>
