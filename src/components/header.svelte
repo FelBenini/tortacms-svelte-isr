45ad3eaf7212329a84bdd1e8d1139dc6e7fb16ba
felbenini <felbenini@gmail.com>
2023-07-28 23:41:53 -0300

    Loading bar

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,3 +1,18 @@
 <header>
-  <p>This is my header</p>
+  <h3><a href='/'>tortaCMS + SvelteKit</a></h3>
 </header>
+
+<style>
+  header {
+    width: 100%;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    margin: 0px;
+    height: 56px;
+    position: fixed;
+    display: flex;
+    background-color: white;
+    z-index: 99;
+    align-items: center;
+    padding: 6px 12px;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,59 @@
 <script>
-	import Header from "../components/header.svelte";
+	import Header from '../components/header.svelte';
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
 
+	import 'nprogress/nprogress.css';
+
+	NProgress.configure({
+		minimum: 0.16,
+    showSpinner: false
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 </script>
-<Header />
-<slot />
 
-<style global >
-  @import url('https://fonts.googleapis.com/css2?family=REM:wght@300;500;700&display=swap');
-  :global(*) {
-    font-family: 'REM', sans-serif;
+<body>
+	<Header />
+	<main>
+		<slot />
+	</main>
+</body>
+
+<style global>
+	@import url('https://fonts.googleapis.com/css2?family=REM:wght@300;500;700&display=swap');
+
+	main {
+		padding-top: 56px;
+	}
+	:global(*) {
+		font-family: 'REM', sans-serif;
+		scrollbar-width: thin;
+	}
+	:global(p) {
+		font-weight: 300;
+	}
+	:global(h1) {
+		font-weight: 500;
+	}
+
+	:global(body) {
+		margin: 0;
+		height: 100%;
+	}
+	:global(html) {
+		height: 100%;
+	}
+
+  :global(#nprogress .bar) {
+    background: rgb(224, 90, 13) !important;
   }
-  :global(p) {
-    font-weight: 300;
-  }
-  :global(h1) {
-    font-weight: 500;
-  }
+
 </style>
