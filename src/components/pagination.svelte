1dcf7a8d5fe6e7276782a8c1ef759561424a6905
felbenini <felbenini@gmail.com>
2023-07-29 02:41:50 -0300

    Styled the pagination component

--- src/components/pagination.svelte
+++ src/components/pagination.svelte
@@ -25,23 +25,34 @@
     width: 100%;
     justify-content: center;
     align-items: center;
-    gap: 6px;
+    margin-top: 12px;
+    gap: 12px;
     & p {
       background-color: rgb(211, 77, 0);
       aspect-ratio: 1/1;
-      width: 20px;
+      width: 28px;
       text-align: center;
+      display: flex;
+      align-items: center;
+      justify-content: center;
       padding: 4px;
       border-radius: 4px;
       color: white;
+      font-weight: 800;
     }
     & a {
-      color: rgb(211, 77, 0);
-      background-color: rgba(211, 77, 0, 0.26);
+      color: rgb(172, 63, 0);
+      background-color: rgba(211, 77, 0, 0.158);
       aspect-ratio: 1/1;
-      width: 20px;
+      width: 28px;
       text-align: center;
+      display: flex;
+      align-items: center;
+      justify-content: center;
       padding: 4px;
+      font-weight: 800;
+      text-decoration: none;
+      border: 1px solid rgba(172, 63, 0, 0.288);
       border-radius: 4px;
     }
   }
